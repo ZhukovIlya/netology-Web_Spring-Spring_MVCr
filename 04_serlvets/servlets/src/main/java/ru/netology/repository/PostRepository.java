6825795a1ae6e71dfe5f43d46d269e1c719ba6a9
Ilya Zhukov <ilyazhukovp@gmail.com>
2021-08-03 11:42:48 +0300

    homework 4.1

--- 04_serlvets/servlets/src/main/java/ru/netology/repository/PostRepository.java
+++ 04_serlvets/servlets/src/main/java/ru/netology/repository/PostRepository.java
@@ -2,24 +2,34 @@ package ru.netology.repository;
 
 import ru.netology.model.Post;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 // Stub
 public class PostRepository {
-  public List<Post> all() {
-    return Collections.emptyList();
-  }
+    private final Map<Long, Post> postMap = new ConcurrentHashMap<>();
+    private final AtomicLong id = new AtomicLong(0);
 
-  public Optional<Post> getById(long id) {
-    return Optional.empty();
-  }
+    public List<Post> all() {
+        return new ArrayList<>(postMap.values());
+    }
 
-  public Post save(Post post) {
-    return post;
-  }
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(postMap.get(id));
+    }
 
-  public void removeById(long id) {
-  }
+    public Post save(Post post) {
+        long id = post.getId();
+        if (id == 0 || id > this.id.get()) {
+            id = this.id.incrementAndGet();
+            post.setId(id);
+        }
+        postMap.put(id, post);
+        return post;
+    }
+
+    public void removeById(long id) {
+        postMap.remove(id);
+    }
 }
